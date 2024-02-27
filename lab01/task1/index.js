f64fdaf88fad78a8d242659182fbf8fda477ca71
Rostislav Glizerin <glizerin1108@gmail.com>
2024-02-28 00:18:29 +0300

    Поправил замечания по 1 лабе

--- lab01/task1/index.js
+++ lab01/task1/index.js
@@ -1,5 +1,7 @@
 window.onload = main
 
+const DEFAULT_FRAME_RATE = 120
+
 function main() {
     const firstLetter = document.body.getElementsByClassName('g-letter')[0]
     const secondLetter = document.body.getElementsByClassName('r-letter')[0]
@@ -9,48 +11,71 @@ function main() {
         throw new Error('one of letter elements is undefined')
     }
 
-    const {animate: animateFirst} = createDropAnimation(firstLetter, 5000/*, 20*/)
-    const {animate: animateSecond}  = createDropAnimation(secondLetter, 1000)
-    const {animate: animateThird}  = createDropAnimation(thirdLetter, 3000/*, 30*/)
+    const {animate: animateFirst} = createDropAnimation(firstLetter, 2000, 400)
+    const {animate: animateSecond}  = createDropAnimation(secondLetter, 1000, 500)
+    const {animate: animateThird}  = createDropAnimation(thirdLetter, 3000, 300)
 
-    const rafLoop = () => {
+    const renderer = () => {
         animateFirst()
         animateSecond()
         animateThird()
-        requestAnimationFrame(rafLoop)
     }
 
-    requestAnimationFrame(rafLoop)
+    render(renderer)
+}
+
+
+function render(renderFn, fpsRate = DEFAULT_FRAME_RATE) {
+    const FRAME_PERIOD = 1000 / fpsRate
+    let lastTime = null
+    const rafHandler = (time) => {
+        if (time - lastTime < FRAME_PERIOD) {
+            requestAnimationFrame(rafHandler);
+            return
+        }
+
+        lastTime = time;
+        renderFn()
+        requestAnimationFrame(rafHandler);
+    }
+
+    requestAnimationFrame(rafHandler)
 }
 
 function createDropAnimation(element, startDelay = 0, gravityAcceleration = 10) {
-    const stepsCount = 50000 * (1 / gravityAcceleration)
     const startTime = Date.now() + startDelay
     let height = parseInt(getComputedStyle(element).bottom, 10)
+    let dropTime = Math.sqrt(2 * height / gravityAcceleration)
+    let prevTime = Date.now() / 1000
 
     return {
         animate: () => {
-            if (startTime > Date.now())
-            {
+            const currentTime = Date.now()
+            if (startTime > currentTime) {
                 return
             }
 
-            const wave = calculateWaveFactor(startTime, stepsCount)
-            const resultH = Math.abs(wave) * height
+            const deltaTime = calculateStep(startTime, prevTime, dropTime)
+            //console.log(deltaTime)
+            const resultH = height - gravityAcceleration * (deltaTime ** 2) / 2
 
             element.setAttribute('style', `bottom: ${resultH}px`);
+            prevTime = currentTime
         },
     }
 }
 
-function calculateWaveFactor(startTime, stepsCount) {
-    const HALF_STEPS_COUNT = stepsCount / 2
-    let step = (Date.now() - startTime) % stepsCount;
+function calculateStep(startTime, prevTime, dropTime) {
+    const DOUBLE_TIME = dropTime * 2
+    let step = (Date.now() - startTime) / 1000 % DOUBLE_TIME
 
-    if (step > HALF_STEPS_COUNT)
-    {
-        step = stepsCount - step
+    if (step > dropTime) {
+        step = dropTime - (step - dropTime)
     }
 
-    return Math.cos(step / HALF_STEPS_COUNT * Math.PI);
+    return step
+}
+
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max)
 }
