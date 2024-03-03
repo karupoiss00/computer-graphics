28ac7ae333516e89509c2b5b939da803e3003d3e
Rostislav Glizerin <glizerin1108@gmail.com>
2024-03-03 15:47:54 +0300

    Поправил замечание по физике в 1 лабе

--- lab01/task1/index.js
+++ lab01/task1/index.js
@@ -1,7 +1,5 @@
 window.onload = main
 
-const DEFAULT_FRAME_RATE = 120
-
 function main() {
     const firstLetter = document.body.getElementsByClassName('g-letter')[0]
     const secondLetter = document.body.getElementsByClassName('r-letter')[0]
@@ -11,9 +9,9 @@ function main() {
         throw new Error('one of letter elements is undefined')
     }
 
-    const {animate: animateFirst} = createDropAnimation(firstLetter, 2000, 400)
-    const {animate: animateSecond}  = createDropAnimation(secondLetter, 1000, 500)
-    const {animate: animateThird}  = createDropAnimation(thirdLetter, 3000, 300)
+    const {animate: animateFirst} = createDropAnimation(firstLetter, 2000, 100)
+    const {animate: animateSecond}  = createDropAnimation(secondLetter, 1000, 40)
+    const {animate: animateThird}  = createDropAnimation(thirdLetter, 3000, 20)
 
     const renderer = () => {
         animateFirst()
@@ -25,16 +23,8 @@ function main() {
 }
 
 
-function render(renderFn, fpsRate = DEFAULT_FRAME_RATE) {
-    const FRAME_PERIOD = 1000 / fpsRate
-    let lastTime = null
-    const rafHandler = (time) => {
-        if (time - lastTime < FRAME_PERIOD) {
-            requestAnimationFrame(rafHandler);
-            return
-        }
-
-        lastTime = time;
+function render(renderFn) {
+    const rafHandler = () => {
         renderFn()
         requestAnimationFrame(rafHandler);
     }
@@ -44,36 +34,38 @@ function render(renderFn, fpsRate = DEFAULT_FRAME_RATE) {
 
 function createDropAnimation(element, startDelay = 0, gravityAcceleration = 10) {
     const startTime = Date.now() + startDelay
-    let height = parseInt(getComputedStyle(element).bottom, 10)
-    let dropTime = Math.sqrt(2 * height / gravityAcceleration)
-    let prevTime = Date.now() / 1000
+    const maxHeight = parseInt(getComputedStyle(element).bottom, 10)
+    let currentHeight = maxHeight
+    let prevTime = null
+    let speed = 0
+
+    const animate = () => {
+        const currentTime = Date.now()
+        if (startTime > currentTime) {
+            return
+        }
+
+        if (!prevTime) {
+            prevTime = currentTime
+        }
+
+        const deltaTime = (currentTime - prevTime) / 1000
+        speed += deltaTime * (-gravityAcceleration)
+        const deltaY = speed * deltaTime
+        currentHeight += deltaY
+
+        const clampedH =  clamp(currentHeight, 0, maxHeight)
+        if (clampedH === 0 || clampedH === maxHeight) {
+            speed = -speed
+        }
+
+        element.setAttribute('style', `bottom: ${currentHeight}px`);
+        prevTime = currentTime
+    }
 
     return {
-        animate: () => {
-            const currentTime = Date.now()
-            if (startTime > currentTime) {
-                return
-            }
-
-            const deltaTime = calculateStep(startTime, prevTime, dropTime)
-            //console.log(deltaTime)
-            const resultH = height - gravityAcceleration * (deltaTime ** 2) / 2
-
-            element.setAttribute('style', `bottom: ${resultH}px`);
-            prevTime = currentTime
-        },
+        animate,
     }
-}
-
-function calculateStep(startTime, prevTime, dropTime) {
-    const DOUBLE_TIME = dropTime * 2
-    let step = (Date.now() - startTime) / 1000 % DOUBLE_TIME
-
-    if (step > dropTime) {
-        step = dropTime - (step - dropTime)
-    }
-
-    return step
 }
 
 function clamp(value, min, max) {
